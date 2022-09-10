4365db9de3acec63db536819380157bfb9ec6e80
Cole Beezley <62979419+onodeloc@users.noreply.github.com>
2022-09-09 22:43:56 -0700

    updated 708 print

--- medium/Add Two Numbers.py
+++ medium/Add Two Numbers.py
@@ -1,25 +1,46 @@
-# initial linked list printing
-# output test
+# Definition for singly-linked list.
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = next
+
+class Link:
+    def __init__(self):
+        self.head = None
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        class Node:
-            def __init__(self, val=0, next=None):
-                self.val = val
-                self.next = next
+        head = None
+        data = None
         
-        class linker:
-            def __init__(self):
-                self.head = None
-
-            def linkerprint(self):
-                toprint = self.head
-                while toprint is not None:
-                    print (toprint.val)
-                    toprint = toprint.next
-                    
-                
-        l1 = linker()
-        l1.head = Node("")
-        l1.linkerprint()
+        l1arr = []
+        l2arr = []
+        cnt = 0
+        
+        while l1 is not None:
+            # print(l1.val)
+            l1arr += [l1.val]
+            l1 = l1.next
+            cnt += 1
+            
+        while l2 is not None:
+            # print(l2.val)
+            l2arr += [l2.val]
+            l2 = l2.next
+            
+        l1val = 0
+        l2val = 0
+        
+        for i in range(cnt):
+            cnt -= 1
+            l1val += l1arr[i]*(10**cnt)
+            l2val += l2arr[i]*(10**cnt)
+        
+        # print(l1val," ",l2val)
+        finalsum = l1val + l2val
+        string = str(finalsum)
+        
+        for i in range(len(string)-1,-1,-1):
+            print(string[i])
+        
