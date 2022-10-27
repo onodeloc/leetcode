6faf44d286372f9a86cc8553606a01f85400f5a0
Cole Beezley <62979419+onodeloc@users.noreply.github.com>
2022-10-26 22:17:36 -0700

    further optimization; removed variables

--- easy/Valid Parentheses.java
+++ easy/Valid Parentheses.java
@@ -1,21 +1,15 @@
 import java.util.Stack;
 //this will need to be commented out for the code to run on LC
 
-// Runtime: 2 ms, faster than 90.92% of Java online submissions for Valid Parentheses.
-// Memory Usage: 40.1 MB, less than 96.56% of Java online submissions for Valid Parentheses.
+// Runtime: 1 ms, faster than 99.78% of Java online submissions for Valid Parentheses.
+// Memory Usage: 40.2 MB, less than 94.20% of Java online submissions for Valid Parentheses.
 
 class Solution {
     public boolean isValid(String s) {
         
         Stack<Integer> stack = new Stack<Integer>(); // the basic premise is FILO to match parentheses
-
-        int hold; // this will hold popped vals
-        boolean noPop; // this will ensure we don't pop an empty stack
         
         for (int i = 0; i < s.length(); i++){
-            
-            noPop = stack.isEmpty() ? true : false;
-            // if stack is empty, noPop = true
             
             // the first three if statements here push assigned values to be checked later
             //  the next three exist after a noPop check, to match the previously inserted values
@@ -32,22 +26,19 @@ class Solution {
             else if (s.charAt(i) == '['){
                 stack.push(3);
             }
-            else if (!noPop){
+            else if (!stack.isEmpty()){
                 if (s.charAt(i) == ')'){
-                    hold = stack.pop();
-                    if (hold != 1){
+                    if (stack.pop() != 1){
                         return false;
                     }
                 } 
                 else if (s.charAt(i) == '}'){
-                    hold = stack.pop();
-                    if (hold != 2){
+                    if (stack.pop() != 2){
                         return false;
                     }
                 }
                 else if (s.charAt(i) == ']'){
-                    hold = stack.pop();
-                    if (hold != 3){
+                    if (stack.pop() != 3){
                         return false;
                     }
                 }
