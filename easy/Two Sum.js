be2793fd1b258309c37f10200756844b9888c474
Cole Beezley <62979419+onodeloc@users.noreply.github.com>
2022-11-09 20:05:04 -0800

    two sum in 5 lines

--- easy/Two Sum.js
+++ easy/Two Sum.js
@@ -1,26 +1,14 @@
+// 26/97
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
  var twoSum = function(nums, target) {
-    
-    var searching = true;
-    var i = 0;
-    var neededNum;
-    const arr = [0,0]
-    
-    while (searching){
-        
-        neededNum = target - nums[i]
-        for (let y = i + 1; y < nums.length; y++){
-            if (nums[y] == neededNum) {
-                arr[0] = i
-                arr[1] = y
-                return arr;
-            }
+    for (let i = 0; i < nums.length; i++){
+        if (nums.includes(target - nums[i]) && (nums.indexOf(target - nums[i]) !== i)){
+            return [i, nums.indexOf(target - nums[i])];
         }
-        i++;
-        
     }
 };
