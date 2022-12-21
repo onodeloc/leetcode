a772ea6164cfb77527600a1256c9dc8c4a24d708
Cole Beezley <cdbbeezley@gmail.com>
2022-12-20 19:27:53 -0800

    added comments

--- easy/Two Sum.py
+++ easy/Two Sum.py
@@ -1,8 +1,22 @@
+# 8/78
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # have array of nums
+        # want to reach target
         
-        for i in range(0,len(nums)):
-            for x in range(i+1,len(nums)):
+        # limitations:
+        # can't use same number twice
+        # there is exactly one solution
+
+        for i in range(0, len(nums)):
+            # iterate through loop
+            # print information
+
+            for x in range(i + 1, len(nums)):
+                # iterate through elements after i
+                # if i + curr matches, return
+
                 if (nums[i] + nums[x] == target):
                     return [i,x]
-        return []
